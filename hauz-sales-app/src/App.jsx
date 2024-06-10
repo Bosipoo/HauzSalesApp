dfd4df2254c25ef7cb73919e15df7085654c6d22
bosipoo <oyinaan@gmail.com>
2024-06-10 06:41:21 +0100

    migration2

--- hauz-sales-app/src/App.jsx
+++ hauz-sales-app/src/App.jsx
@@ -1,33 +1,22 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Layout from './components/Layout'
 import Login from './pages/Login'
 import Home from './pages/Home'
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
-import GeneralLedgers from './pages/GeneralLedgers';
+import GeneralLedgers from './pages/GeneralLedgers'
 import './App.css'
 
 function App() {
-  const [openSidebarToggle, setOpenSidebarToogle] = useState(false)
-
-  const OpenSidebar = () => {
-    setOpenSidebarToogle(!openSidebarToggle);
-  };
-
+  const [count, setCount] = useState(0)
 
   return (
-    <div className='grid-container'>
-      <Header OpenSidebar={OpenSidebar} />
-      <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar} />
-      <Router>
-          <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route path="/" element={<Home />} />
-              <Route path="/general-ledgers" element={<GeneralLedgers />} />
-          </Routes>
-      </Router>
-    </div>
+    <Router>
+        <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Layout><Home /></Layout>}/>
+            <Route path="/general-ledgers" element={<Layout><GeneralLedgers /></Layout>} />
+        </Routes>
+    </Router>
   )
 }
 
