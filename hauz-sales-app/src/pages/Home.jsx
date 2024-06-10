38dd90d3ac09169baef2066ad210df6d995a90ae
bosipoo <oyinaan@gmail.com>
2024-06-10 06:53:16 +0100

    migrationTest2

--- hauz-sales-app/src/pages/Home.jsx
+++ hauz-sales-app/src/pages/Home.jsx
@@ -50,90 +50,94 @@ const Home = () => {
         },
       ];
     return (
-        <>
-        <main className='main-container'>
-            <div className='main-title'>
-                <h2>DASHBOARD</h2>
-            </div>
+        // <>
+        // <main className='main-container'>
+        //     <div className='main-title'>
+        //         <h2>DASHBOARD</h2>
+        //     </div>
 
-            <div className='main-cards'>
-                <div className='card'>
-                    <div className='card-inner'>
-                        <h3> PROPERTIES </h3>
-                        <BsBank2 className='card-icon' />
-                    </div>
-                    <h1>300</h1>
-                </div>
-                <div className='card'>
-                    <div className='card-inner'>
-                        <h3> SALES </h3>
-                        <BsBasketFill className='card-icon' />
-                    </div>
-                    <h1>7</h1>
-                </div>
-                <div className='card'>
-                    <div className='card-inner'>
-                        <h3> TRANSACTIONS </h3>
-                        <BsBuildingFillUp className='card-icon' />
-                    </div>
-                    <h1>13</h1>
-                </div>
-                <div className='card'>
-                    <div className='card-inner'>
-                        <h3> SALES REPS </h3>
-                        <BsPeopleFill className='card-icon' />
-                    </div>
-                    <h1>30</h1>
-                </div>
-            </div>
+        //     <div className='main-cards'>
+        //         <div className='card'>
+        //             <div className='card-inner'>
+        //                 <h3> PROPERTIES </h3>
+        //                 <BsBank2 className='card-icon' />
+        //             </div>
+        //             <h1>300</h1>
+        //         </div>
+        //         <div className='card'>
+        //             <div className='card-inner'>
+        //                 <h3> SALES </h3>
+        //                 <BsBasketFill className='card-icon' />
+        //             </div>
+        //             <h1>7</h1>
+        //         </div>
+        //         <div className='card'>
+        //             <div className='card-inner'>
+        //                 <h3> TRANSACTIONS </h3>
+        //                 <BsBuildingFillUp className='card-icon' />
+        //             </div>
+        //             <h1>13</h1>
+        //         </div>
+        //         <div className='card'>
+        //             <div className='card-inner'>
+        //                 <h3> SALES REPS </h3>
+        //                 <BsPeopleFill className='card-icon' />
+        //             </div>
+        //             <h1>30</h1>
+        //         </div>
+        //     </div>
 
-            <div className='charts'>
-                <ResponsiveContainer width="100%" height="100%">
-                    <BarChart
-                    width={500}
-                    height={300}
-                    data={data}
-                    margin={{
-                        top: 5,
-                        right: 30,
-                        left: 20,
-                        bottom: 5,
-                    }}
-                    >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Bar dataKey="pv" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
-                    <Bar dataKey="uv" fill="#82ca9d" activeBar={<Rectangle fill="gold" stroke="purple" />} />
-                    </BarChart>
-                </ResponsiveContainer>
+        //     <div className='charts'>
+        //         <ResponsiveContainer width="100%" height="100%">
+        //             <BarChart
+        //             width={500}
+        //             height={300}
+        //             data={data}
+        //             margin={{
+        //                 top: 5,
+        //                 right: 30,
+        //                 left: 20,
+        //                 bottom: 5,
+        //             }}
+        //             >
+        //             <CartesianGrid strokeDasharray="3 3" />
+        //             <XAxis dataKey="name" />
+        //             <YAxis />
+        //             <Tooltip />
+        //             <Legend />
+        //             <Bar dataKey="pv" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
+        //             <Bar dataKey="uv" fill="#82ca9d" activeBar={<Rectangle fill="gold" stroke="purple" />} />
+        //             </BarChart>
+        //         </ResponsiveContainer>
 
-                <ResponsiveContainer width="100%" height="100%">
-                    <LineChart
-                    width={500}
-                    height={300}
-                    data={data}
-                    margin={{
-                        top: 5,
-                        right: 30,
-                        left: 20,
-                        bottom: 5,
-                    }}
-                    >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-                    <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-                    </LineChart>
-                </ResponsiveContainer>
-            </div>
-        </main>
-        </>
+        //         <ResponsiveContainer width="100%" height="100%">
+        //             <LineChart
+        //             width={500}
+        //             height={300}
+        //             data={data}
+        //             margin={{
+        //                 top: 5,
+        //                 right: 30,
+        //                 left: 20,
+        //                 bottom: 5,
+        //             }}
+        //             >
+        //             <CartesianGrid strokeDasharray="3 3" />
+        //             <XAxis dataKey="name" />
+        //             <YAxis />
+        //             <Tooltip />
+        //             <Legend />
+        //             <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+        //             <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        //             </LineChart>
+        //         </ResponsiveContainer>
+        //     </div>
+        // </main>
+        // </>
+        <div>
+            <h2>Home Page</h2>
+
+        </div>
     )
 }
 
