2b85f805a60b150bd45aac7ff10196a3147f9db0
bosipoo <oyinaan@gmail.com>
2024-06-10 07:13:52 +0100

    newProject

---
+++ hauz-test/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Router>
+        <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Home />} />
+        </Routes>
+    </Router>
+  )
+}
+
+export default App
