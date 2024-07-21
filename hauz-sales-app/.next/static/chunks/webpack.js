dbb5be2a24de6fef8e79b804de12ad2b72848481
bosipoo <oyinaan@gmail.com>
2024-07-21 06:20:52 +0100

    deployment config

--- hauz-sales-app/.next/static/chunks/webpack.js
+++ hauz-sales-app/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f2074ffc109ea8b9"; }
+/******/ 		__webpack_require__.h = function() { return "e994d4ae64786834"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
